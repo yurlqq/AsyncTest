7e089b7849fa78ef6493ab7951a05d8753a7dd60
yurlqq <inseu99@naver.com>
2023-01-05 10:57:15 +0900

    Initial Commit

---
+++ AsyncTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AsyncTest
+//
+//  Created by 박인서 on 2023/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
